9134b0365f12a43f66ed86cb082da75b3f936399
Valeriy Khoma <bergamolt22@gmail.com>
2024-07-04 00:51:58 +0300

    Update AppRoot component in layout.tsx

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <AppRoot appearance='dark' platform='ios'>
+        <AppRoot>
           <TelegramProvider>{children}</TelegramProvider>
         </AppRoot>
       </body>
