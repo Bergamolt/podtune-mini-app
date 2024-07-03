5ac40e0d0c7cb552288e911191a72fe97e3bc3c0
Valeriy Khoma <bergamolt22@gmail.com>
2024-07-04 00:17:41 +0300

    Fix build

--- app/app/(pages)/podcast/[id]/page.tsx
+++ app/app/(pages)/podcast/[id]/page.tsx
@@ -22,7 +22,7 @@ export default async function PodcastPage({ params }: PodcastPageProps) {
         <h3 className='text-xl font-semibold mt-4'>{podcast.feed.title}</h3>
         <p className='text-gray-500 mt-2'>{podcast.feed.description}</p>
       </div>
-      {epicodes.items.map((episode) => (
+      {epicodes.items.map((episode: any) => (
         <div
           key={episode.id}
           className='bg-white shadow-sm rounded-md p-4 mt-4'
--- app/podcast-index-api/index.js
+++ app/podcast-index-api/index.js
@@ -1,41 +1,41 @@
-import querystring from 'querystring';
-import {sha1} from 'js-sha1';
+import querystring from 'querystring'
+import { sha1 } from 'js-sha1'
 
 function generateAuthHeader(xAuthKey, apiSecret, xAuthDate) {
-  const sha1Hash = sha1.update(xAuthKey + apiSecret + xAuthDate).hex();
+  const sha1Hash = sha1.update(xAuthKey + apiSecret + xAuthDate).hex()
 
-  return sha1Hash;
+  return sha1Hash
 }
 
-const BASE_API_URL = 'https://api.podcastindex.org/api/1.0/';
+const BASE_API_URL = 'https://api.podcastindex.org/api/1.0/'
 
-const PATH_SEARCH_BY_TERM = 'search/byterm';
-const PATH_SEARCH_BY_TITLE = 'search/bytitle';
-const PATH_SEARCH_EPISODE_BY_PERSON = 'search/byperson';
-const PATH_ADD_BY_FEED_URL = 'add/byfeedurl';
-const PATH_ADD_BY_ITUNES_ID = 'add/byitunesid';
-const PATH_EPISODES_BY_FEED_ID = 'episodes/byfeedid';
-const PATH_EPISODES_BY_FEED_URL = 'episodes/byfeedurl';
-const PATH_EPISODES_BY_ITUNES_ID = 'episodes/byitunesid';
-const PATH_EPISODES_BY_ID = 'episodes/byid';
-const PATH_EPISODES_RANDOM = 'episodes/random';
-const PATH_PODCASTS_BY_FEED_URL = 'podcasts/byfeedurl';
-const PATH_PODCASTS_BY_FEED_ID = 'podcasts/byfeedid';
-const PATH_PODCASTS_BY_ITUNES_ID = 'podcasts/byitunesid';
-const PATH_PODCASTS_BY_GUID = 'podcasts/byguid';
-const PATH_PODCASTS_BY_TAG = 'podcasts/bytag';
-const PATH_PODCASTS_TRENDING = 'podcasts/trending';
-const PATH_PODCASTS_DEAD = 'podcasts/dead';
-const PATH_RECENT_FEEDS = 'recent/feeds';
-const PATH_RECENT_EPISODES = 'recent/episodes';
-const PATH_RECENT_NEWFEEDS = 'recent/newfeeds';
-const PATH_RECENT_SOUNDBITES = 'recent/soundbites';
-const PATH_VALUE_BY_FEED_ID = 'value/byfeedid';
-const PATH_VALUE_BY_FEED_URL = 'value/byfeedurl';
-const PATH_CATEGORIES_LIST = 'categories/list';
-const PATH_HUB_PUBNOTIFIY = 'hub/pubnotify';
+const PATH_SEARCH_BY_TERM = 'search/byterm'
+const PATH_SEARCH_BY_TITLE = 'search/bytitle'
+const PATH_SEARCH_EPISODE_BY_PERSON = 'search/byperson'
+const PATH_ADD_BY_FEED_URL = 'add/byfeedurl'
+const PATH_ADD_BY_ITUNES_ID = 'add/byitunesid'
+const PATH_EPISODES_BY_FEED_ID = 'episodes/byfeedid'
+const PATH_EPISODES_BY_FEED_URL = 'episodes/byfeedurl'
+const PATH_EPISODES_BY_ITUNES_ID = 'episodes/byitunesid'
+const PATH_EPISODES_BY_ID = 'episodes/byid'
+const PATH_EPISODES_RANDOM = 'episodes/random'
+const PATH_PODCASTS_BY_FEED_URL = 'podcasts/byfeedurl'
+const PATH_PODCASTS_BY_FEED_ID = 'podcasts/byfeedid'
+const PATH_PODCASTS_BY_ITUNES_ID = 'podcasts/byitunesid'
+const PATH_PODCASTS_BY_GUID = 'podcasts/byguid'
+const PATH_PODCASTS_BY_TAG = 'podcasts/bytag'
+const PATH_PODCASTS_TRENDING = 'podcasts/trending'
+const PATH_PODCASTS_DEAD = 'podcasts/dead'
+const PATH_RECENT_FEEDS = 'recent/feeds'
+const PATH_RECENT_EPISODES = 'recent/episodes'
+const PATH_RECENT_NEWFEEDS = 'recent/newfeeds'
+const PATH_RECENT_SOUNDBITES = 'recent/soundbites'
+const PATH_VALUE_BY_FEED_ID = 'value/byfeedid'
+const PATH_VALUE_BY_FEED_URL = 'value/byfeedurl'
+const PATH_CATEGORIES_LIST = 'categories/list'
+const PATH_HUB_PUBNOTIFIY = 'hub/pubnotify'
 
-const qs = o => '?' + querystring.stringify(o);
+const qs = (o) => '?' + querystring.stringify(o)
 
 /**
  * Processes the response from the API.
@@ -43,19 +43,19 @@ const qs = o => '?' + querystring.stringify(o);
  * @returns {Promise<Object>} - The parsed JSON body of the response.
  * @throws {Object} - An error object if the request failed.
  */
-const withResponse = async response => {
-  const body = await response.json();
+const withResponse = async (response) => {
+  const body = await response.json()
   if (
     response.status === 500 ||
     (body.hasOwnProperty('status') && body.status === 'false')
   ) {
     const message = body.hasOwnProperty('description')
       ? body.description
-      : 'Request failed.';
-    throw {message, code: response.status};
+      : 'Request failed.'
+    throw { message, code: response.status }
   }
-  return body;
-};
+  return body
+}
 
 /**
  * Custom fetch function with authentication headers.
@@ -67,41 +67,69 @@ const withResponse = async response => {
  * @returns {Promise<Object>} - The JSON response from the API.
  */
 const customFetch = async (path, queries, key, secret, userAgent) => {
-  const dt = Math.floor(Date.now() / 1000).toString();
+  const dt = Math.floor(Date.now() / 1000).toString()
 
   const headers = {
     'User-Agent': userAgent || 'PodcastIndexBot/@podcast@noagendasocial.com',
     'X-Auth-Date': dt,
     'X-Auth-Key': key,
     Authorization: generateAuthHeader(key, secret, dt),
-  };
-
-  const url = `${BASE_API_URL}${path}${qs(queries)}`;
-  const response = await fetch(url, {headers});
-
-  if (!response.ok) {
-    return withResponse(response);
   }
 
-  return await response.json();
-};
+  const url = `${BASE_API_URL}${path}${qs(queries)}`
+  const response = await fetch(url, { headers })
+
+  if (!response.ok) {
+    return withResponse(response)
+  }
+
+  return await response.json()
+}
 
 /**
  * Creates a PodcastIndex API client.
  * @param {string} key - The API key.
  * @param {string} secret - The API secret.
  * @param {string} [userAgent] - The user agent string.
- * @returns {Object} - The PodcastIndex API client.
+ * @returns {
+ *  searchByTerm: Function,
+ *  searchByTitle: Function,
+ *  searchEpisodesByPerson: Function,
+ *  podcastsByFeedUrl: Function,
+ *  podcastsByFeedId: Function,
+ *  podcastsByFeedItunesId: Function,
+ *  podcastsByGUID: Function,
+ *  podcastsByTag: Function,
+ *  podcastsTrending: Function,
+ *  podcastsDead: Function,
+ *  addByFeedUrl: Function,
+ *  addByItunesId: Function,
+ *  episodesByFeedId: Function,
+ *  episodesByFeedUrl: Function,
+ *  episodesByItunesId: Function,
+ *  episodesById: Function,
+ *  episodesRandom: Function,
+ *  recentFeeds: Function,
+ *  recentEpisodes: Function,
+ *  recentNewFeeds: Function,
+ *  recentSoundbites: Function,
+ *  valueByFeedId: Function,
+ *  valueByFeedUrl: Function,
+ *  categoriesList: Function,
+ *  hubPubNotifyById: Function,
+ *  hubPubNotifyByUrl: Function,
+ *  custom: Function
+ * } - The PodcastIndex API client.
  */
 export const podcastIndexApi = (key, secret, userAgent) => {
   if (!key || !secret) {
     throw new Error(
-      'API Key and Secret are required from https://api.podcastindex.org/',
-    );
+      'API Key and Secret are required from https://api.podcastindex.org/'
+    )
   }
 
   const custom = async (path, queries) =>
-    customFetch(path, queries, key, secret, userAgent);
+    customFetch(path, queries, key, secret, userAgent)
 
   return {
     custom,
@@ -115,17 +143,17 @@ export const podcastIndexApi = (key, secret, userAgent) => {
      * @returns {Promise<Object>} - The search results.
      */
     searchByTerm: async (q, val = '', clean = false, fullText = false) => {
-      const queries = {q};
+      const queries = { q }
       if (val !== '') {
-        queries.val = val;
+        queries.val = val
       }
       if (clean) {
-        queries.clean = '';
+        queries.clean = ''
       }
       if (fullText) {
-        queries.fullText = '';
+        queries.fullText = ''
       }
-      return custom(PATH_SEARCH_BY_TERM, queries);
+      return custom(PATH_SEARCH_BY_TERM, queries)
     },
 
     /**
@@ -137,17 +165,17 @@ export const podcastIndexApi = (key, secret, userAgent) => {
      * @returns {Promise<Object>} - The search results.
      */
     searchByTitle: async (q, val = '', clean = false, fullText = false) => {
-      const queries = {q};
+      const queries = { q }
       if (val !== '') {
-        queries.val = val;
+        queries.val = val
       }
       if (clean) {
-        queries.clean = '';
+        queries.clean = ''
       }
       if (fullText) {
-        queries.fullText = '';
+        queries.fullText = ''
       }
-      return custom(PATH_SEARCH_BY_TITLE, queries);
+      return custom(PATH_SEARCH_BY_TITLE, queries)
     },
 
     /**
@@ -157,11 +185,11 @@ export const podcastIndexApi = (key, secret, userAgent) => {
      * @returns {Promise<Object>} - The search results.
      */
     searchEpisodesByPerson: async (q, fullText = false) => {
-      const queries = {q};
+      const queries = { q }
       if (fullText) {
-        queries.fullText = '';
+        queries.fullText = ''
       }
-      return custom(PATH_SEARCH_EPISODE_BY_PERSON, queries);
+      return custom(PATH_SEARCH_EPISODE_BY_PERSON, queries)
     },
 
     /**
@@ -169,38 +197,38 @@ export const podcastIndexApi = (key, secret, userAgent) => {
      * @param {string} feedUrl - The feed URL.
      * @returns {Promise<Object>} - The podcast data.
      */
-    podcastsByFeedUrl: async feedUrl =>
-      custom(PATH_PODCASTS_BY_FEED_URL, {url: feedUrl}),
+    podcastsByFeedUrl: async (feedUrl) =>
+      custom(PATH_PODCASTS_BY_FEED_URL, { url: feedUrl }),
 
     /**
      * Gets podcasts by feed ID.
      * @param {string} feedId - The feed ID.
      * @returns {Promise<Object>} - The podcast data.
      */
-    podcastsByFeedId: async feedId =>
-      custom(PATH_PODCASTS_BY_FEED_ID, {id: feedId}),
+    podcastsByFeedId: async (feedId) =>
+      custom(PATH_PODCASTS_BY_FEED_ID, { id: feedId }),
 
     /**
      * Gets podcasts by iTunes ID.
      * @param {string} itunesId - The iTunes ID.
      * @returns {Promise<Object>} - The podcast data.
      */
-    podcastsByFeedItunesId: async itunesId =>
-      custom(PATH_PODCASTS_BY_ITUNES_ID, {id: itunesId}),
+    podcastsByFeedItunesId: async (itunesId) =>
+      custom(PATH_PODCASTS_BY_ITUNES_ID, { id: itunesId }),
 
     /**
      * Gets podcasts by GUID.
      * @param {string} guid - The GUID.
      * @returns {Promise<Object>} - The podcast data.
      */
-    podcastsByGUID: async guid => custom(PATH_PODCASTS_BY_GUID, {guid}),
+    podcastsByGUID: async (guid) => custom(PATH_PODCASTS_BY_GUID, { guid }),
 
     /**
      * Gets podcasts by tag.
      * @returns {Promise<Object>} - The podcast data.
      */
     podcastsByTag: async () =>
-      custom(PATH_PODCASTS_BY_TAG, {'podcast-value': ''}),
+      custom(PATH_PODCASTS_BY_TAG, { 'podcast-value': '' }),
 
     /**
      * Gets trending podcasts.
@@ -216,10 +244,10 @@ export const podcastIndexApi = (key, secret, userAgent) => {
       since = null,
       lang = null,
       cat = null,
-      notcat = null,
+      notcat = null
     ) => {
-      const queries = {max, since, lang, cat, notcat};
-      return custom(PATH_PODCASTS_TRENDING, queries);
+      const queries = { max, since, lang, cat, notcat }
+      return custom(PATH_PODCASTS_TRENDING, queries)
     },
 
     /**
@@ -236,8 +264,8 @@ export const podcastIndexApi = (key, secret, userAgent) => {
      * @returns {Promise<Object>} - The result of the add operation.
      */
     addByFeedUrl: async (feedUrl, chash = null, itunesId = null) => {
-      const queries = {url: feedUrl, chash, itunesid: itunesId};
-      return custom(PATH_ADD_BY_FEED_URL, queries);
+      const queries = { url: feedUrl, chash, itunesid: itunesId }
+      return custom(PATH_ADD_BY_FEED_URL, queries)
     },
 
     /**
@@ -245,8 +273,8 @@ export const podcastIndexApi = (key, secret, userAgent) => {
      * @param {string} itunesId - The iTunes ID.
      * @returns {Promise<Object>} - The result of the add operation.
      */
-    addByItunesId: async itunesId =>
-      custom(PATH_ADD_BY_ITUNES_ID, {id: itunesId}),
+    addByItunesId: async (itunesId) =>
+      custom(PATH_ADD_BY_ITUNES_ID, { id: itunesId }),
 
     /**
      * Gets episodes by feed ID.
@@ -260,13 +288,13 @@ export const podcastIndexApi = (key, secret, userAgent) => {
       feedId,
       since = null,
       max = 10,
-      fullText = false,
+      fullText = false
     ) => {
-      const queries = {id: feedId, since, max};
+      const queries = { id: feedId, since, max }
       if (fullText) {
-        queries.fullText = '';
+        queries.fullText = ''
       }
-      return custom(PATH_EPISODES_BY_FEED_ID, queries);
+      return custom(PATH_EPISODES_BY_FEED_ID, queries)
     },
 
     /**
@@ -281,13 +309,13 @@ export const podcastIndexApi = (key, secret, userAgent) => {
       feedUrl,
       since = null,
       max = 10,
-      fullText = false,
+      fullText = false
     ) => {
-      const queries = {url: feedUrl, since, max};
+      const queries = { url: feedUrl, since, max }
       if (fullText) {
-        queries.fullText = '';
+        queries.fullText = ''
       }
-      return custom(PATH_EPISODES_BY_FEED_URL, queries);
+      return custom(PATH_EPISODES_BY_FEED_URL, queries)
     },
 
     /**
@@ -302,13 +330,13 @@ export const podcastIndexApi = (key, secret, userAgent) => {
       itunesId,
       since = null,
       max = 10,
-      fullText = false,
+      fullText = false
     ) => {
-      const queries = {id: itunesId, since, max};
+      const queries = { id: itunesId, since, max }
       if (fullText) {
-        queries.fullText = '';
+        queries.fullText = ''
       }
-      return custom(PATH_EPISODES_BY_ITUNES_ID, queries);
+      return custom(PATH_EPISODES_BY_ITUNES_ID, queries)
     },
 
     /**
@@ -318,11 +346,11 @@ export const podcastIndexApi = (key, secret, userAgent) => {
      * @returns {Promise<Object>} - The episode.
      */
     episodesById: async (id, fullText = false) => {
-      const queries = {id};
+      const queries = { id }
       if (fullText) {
-        queries.fullText = '';
+        queries.fullText = ''
       }
-      return custom(PATH_EPISODES_BY_ID, queries);
+      return custom(PATH_EPISODES_BY_ID, queries)
     },
 
     /**
@@ -339,13 +367,13 @@ export const podcastIndexApi = (key, secret, userAgent) => {
       lang = null,
       cat = null,
       notcat = null,
-      fullText = false,
+      fullText = false
     ) => {
-      const queries = {max, lang, cat, notcat};
+      const queries = { max, lang, cat, notcat }
       if (fullText) {
-        queries.fullText = '';
+        queries.fullText = ''
       }
-      return custom(PATH_EPISODES_RANDOM, queries);
+      return custom(PATH_EPISODES_RANDOM, queries)
     },
 
     /**
@@ -362,10 +390,10 @@ export const podcastIndexApi = (key, secret, userAgent) => {
       since = null,
       cat = null,
       lang = null,
-      notcat = null,
+      notcat = null
     ) => {
-      const queries = {max, since, lang, cat, notcat};
-      return custom(PATH_RECENT_FEEDS, queries);
+      const queries = { max, since, lang, cat, notcat }
+      return custom(PATH_RECENT_FEEDS, queries)
     },
 
     /**
@@ -380,13 +408,13 @@ export const podcastIndexApi = (key, secret, userAgent) => {
       max = 10,
       excludeString = null,
       before = null,
-      fullText = false,
+      fullText = false
     ) => {
-      const queries = {max, excludeString, before};
+      const queries = { max, excludeString, before }
       if (fullText) {
-        queries.fullText = '';
+        queries.fullText = ''
       }
-      return custom(PATH_RECENT_EPISODES, queries);
+      return custom(PATH_RECENT_EPISODES, queries)
     },
 
     /**
@@ -396,29 +424,31 @@ export const podcastIndexApi = (key, secret, userAgent) => {
      * @returns {Promise<Object>} - The recent new feeds.
      */
     recentNewFeeds: async (max = 20, since = null) =>
-      custom(PATH_RECENT_NEWFEEDS, {max, since}),
+      custom(PATH_RECENT_NEWFEEDS, { max, since }),
 
     /**
      * Gets recent soundbites.
      * @param {number} [max=20] - Maximum number of results.
      * @returns {Promise<Object>} - The recent soundbites.
      */
-    recentSoundbites: async (max = 20) => custom(PATH_RECENT_SOUNDBITES, {max}),
+    recentSoundbites: async (max = 20) =>
+      custom(PATH_RECENT_SOUNDBITES, { max }),
 
     /**
      * Gets value by feed ID.
      * @param {string} feedId - The feed ID.
      * @returns {Promise<Object>} - The value.
      */
-    valueByFeedId: async feedId => custom(PATH_VALUE_BY_FEED_ID, {id: feedId}),
+    valueByFeedId: async (feedId) =>
+      custom(PATH_VALUE_BY_FEED_ID, { id: feedId }),
 
     /**
      * Gets value by feed URL.
      * @param {string} feedUrl - The feed URL.
      * @returns {Promise<Object>} - The value.
      */
-    valueByFeedUrl: async feedUrl =>
-      custom(PATH_VALUE_BY_FEED_URL, {url: feedUrl}),
+    valueByFeedUrl: async (feedUrl) =>
+      custom(PATH_VALUE_BY_FEED_URL, { url: feedUrl }),
 
     /**
      * Gets the list of categories.
@@ -431,14 +461,15 @@ export const podcastIndexApi = (key, secret, userAgent) => {
      * @param {string} feedId - The feed ID.
      * @returns {Promise<Object>} - The result of the notification.
      */
-    hubPubNotifyById: async feedId => custom(PATH_HUB_PUBNOTIFIY, {id: feedId}),
+    hubPubNotifyById: async (feedId) =>
+      custom(PATH_HUB_PUBNOTIFIY, { id: feedId }),
 
     /**
      * Publishes a notification to the hub by feed URL.
      * @param {string} feedUrl - The feed URL.
      * @returns {Promise<Object>} - The result of the notification.
      */
-    hubPubNotifyByUrl: async feedUrl =>
-      custom(PATH_HUB_PUBNOTIFIY, {url: feedUrl}),
-  };
-};
+    hubPubNotifyByUrl: async (feedUrl) =>
+      custom(PATH_HUB_PUBNOTIFIY, { url: feedUrl }),
+  }
+}
