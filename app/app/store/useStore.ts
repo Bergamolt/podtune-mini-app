301f794ee58092466b4e911e413b843fe8f12462
Valeriy Khoma <bergamolt22@gmail.com>
2024-07-13 22:20:49 +0300

    refactor: Add useContinueListening and useListeningEpisode hooks to useInitStore
    This commit adds the `useContinueListening` and `useListeningEpisode` hooks to the `useInitStore` function in `useStore.ts`. These hooks are used to load the active episode and continue listening episodes respectively. By including these hooks in the `useInitStore` function, we ensure that the necessary data is loaded when the web app is initialized. This improves the overall functionality and user experience of the app.

--- app/app/store/useStore.ts
+++ app/app/store/useStore.ts
@@ -1,14 +1,22 @@
 import { useEffect, useContext } from 'react'
 import { TelegramContext } from '../TelegramProvider'
 import { useFavoritePodcasts } from './useFavoritePodcasts'
+import { useContinueListening } from './useContinueListening'
+import { useListeningEpisode } from './useListeningEpisode'
 
 export const useInitStore = () => {
   const { webApp } = useContext(TelegramContext)
   const loadFavorites = useFavoritePodcasts((state) => state.loadFavorites)
+  const loadEpisodeActive = useListeningEpisode((state) => state.loadEpisode)
+  const loadContinueListening = useContinueListening(
+    (state) => state.loadEpisodes
+  )
 
   useEffect(() => {
     if (webApp) {
       loadFavorites()
+      loadEpisodeActive()
+      loadContinueListening()
     }
-  }, [webApp, loadFavorites])
+  }, [webApp, loadFavorites, loadEpisodeActive, loadContinueListening])
 }
