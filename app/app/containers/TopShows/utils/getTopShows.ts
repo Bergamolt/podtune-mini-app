55bf78108cc5fc62cec3e9d6eb873f6adf50b975
Valeriy Khoma <bergamolt22@gmail.com>
2024-07-26 12:27:00 +0300

    For test

--- app/app/containers/TopShows/utils/getTopShows.ts
+++ app/app/containers/TopShows/utils/getTopShows.ts
@@ -1,23 +1,22 @@
-import { cache } from 'react'
 import { PodcastsTrendingResponse } from '@/app/types'
 import { podcastIndex } from '@/app/utils/podcastIndex'
 import { headers } from 'next/headers'
 import { getLanguageCode } from '@/app/utils/getLanguageCode'
 
-export const getTopShows = cache(
-  async (): Promise<PodcastsTrendingResponse['feeds']> => {
-    const country = headers().get('x-geo-country')
-    const lang = getLanguageCode(country)
+export const getTopShows = async (): Promise<
+  PodcastsTrendingResponse['feeds']
+> => {
+  const country = headers().get('x-geo-country')
+  const lang = getLanguageCode(country)
+  console.log('lang', lang, country)
+  let data = await podcastIndex.podcastsTrending(12, null, lang + 2)
 
-    let data = await podcastIndex.podcastsTrending(12, null, lang + 2)
-
-    if (data.feeds.length) {
-      return data.feeds
-    }
-
-    // Fallback if no data is returned for the country
-    data = await podcastIndex.podcastsTrending(12, null, 'en')
-
+  if (data.feeds.length) {
     return data.feeds
   }
-)
+
+  // Fallback if no data is returned for the country
+  data = await podcastIndex.podcastsTrending(12, null, 'en')
+
+  return data.feeds
+}
